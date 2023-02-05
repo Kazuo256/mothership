213e611c925b8469517025a6875be1c355e4346a
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2023-02-04 22:17:45 -0300

    All collisions

--- space/entity/property/destruction.gd
+++ space/entity/property/destruction.gd
@@ -4,8 +4,13 @@ signal destroyed
 
 @export var hit_points := 1
 
+@onready var is_destroyed := false
+
 func _take_hit(damage: int):
 	if hit_points > 0:
 		hit_points = max(hit_points - damage, 0)
-		if hit_points == 0:
-			destroyed.emit()
+
+func _physics_process(_delta):
+	if hit_points == 0 and not is_destroyed:
+		is_destroyed = true
+		destroyed.emit()
--- space/entity/property/hitbox.gd
+++ space/entity/property/hitbox.gd
@@ -1,11 +1,10 @@
 class_name Hitbox extends Area3D
 
-signal hit(target: Node3D)
+@export var power := 1
 
 func _ready():
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Node3D):
 	if area is Hurtbox:
-		hit.emit(area.owner)
-		owner.queue_free()
+		area.owner.propagate_call('_take_hit', [power])
