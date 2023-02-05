05e01d370ff511ce56df720f49e5be2024084598
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2023-02-05 17:10:50 -0300

    Add alternative keyboard+mouse controls

--- space/entity/property/player_input.gd
+++ space/entity/property/player_input.gd
@@ -3,12 +3,17 @@ class_name PlayerInput extends Node3D
 @export var ship_controller: ShipController
 
 func _physics_process(_delta):
-	ship_controller.shooting_dir = Input.get_vector(
-			"player_shoot_left",
-			"player_shoot_right",
-			"player_shoot_up",
-			"player_shoot_down"
-	).normalized()
+	if Input.get_joy_name(0).is_empty():
+		var mouse_pos := get_viewport().get_mouse_position()
+		var center_pos := get_viewport().get_visible_rect().get_center()
+		ship_controller.shooting_dir = (mouse_pos - center_pos).normalized()
+	else:
+		ship_controller.shooting_dir = Input.get_vector(
+				"player_shoot_left",
+				"player_shoot_right",
+				"player_shoot_up",
+				"player_shoot_down"
+		).normalized()
 	ship_controller.frontal_control = \
 			Input.get_axis("player_backward", "player_forward")
 	ship_controller.steer_control = \
